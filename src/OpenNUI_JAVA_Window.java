c889b9af59f8f12b23429e398a27aab3bb859c18
schoan <schoani@gmail.com>
2016-01-11 03:05:16 +0900

    + constraints.

--- src/OpenNUI_JAVA_Window.java
+++ src/OpenNUI_JAVA_Window.java
@@ -2,14 +2,44 @@ import java.awt.*;
 import javax.swing.*;
 
 
-public class OpenNUI_JAVA_Window extends JFrame{
+public class OpenNUI_JAVA_Window extends JFrame
+{
 	
-	public OpenNUI_JAVA_Window(){
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	public OpenNUI_JAVA_Window()
+	{
+		Container container = getContentPane();
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screenSize = tk.getScreenSize();
 		System.out.println("해상도 : " + screenSize.width + "x" + screenSize.height);
+		
+		Panel panel = new Panel();
+		panel.setLayout(new BorderLayout());
+		container.add(panel);
+		
+		MediaTracker mt = new MediaTracker(this);
+		mt.addImage(tk.getImage("\\image\\logo.png"), 0);
+		JImagePanel imagePanel = new JImagePanel(tk.getImage("\\image\\logo.png"));
+		panel.add(imagePanel, BorderLayout.NORTH);
+		
+		
 		this.setSize(screenSize.width-50, screenSize.height-100);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	class JImagePanel extends JPanel
+	{
+		private Image image;
+		public JImagePanel(Image img)
+		{
+			this.image = img;
+		}
+		
+		public void paintComponent(Graphics g)
+		{
+			g.drawImage(this.image, 0, 0, this.getWidth(), this.getHeight(), this);
+		}
 	}
 	
 	//임시 메인파일.
